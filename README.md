ca58fa9cef2cc188e9a3fc99de7ace3d5b32162e
Manan Patel <manan19@gmail.com>
2021-06-14 01:18:20 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# map-share
+App &amp; plugin for map sharing on dark forest
+
+
+## Running the server
+```
+npm install
+node app.js
+```
+
+## Using the plugin
+Make sure to replace `this.url` in the extended plugin with appropriate shared url
---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+app.use(cors());
+
+const port = 8500
+const mapFile = 'map.json'
+
+//////
+// App logic
+function hashOfChunkFootprint(chunkFootprint) {
+  return md5(JSON.stringify(chunkFootprint));
+}
+
+function convertChunksArrayToChunkMap(chunksArray) {
+  let chunksMap = {};
+  for (let chunksArrayElement of chunksArray) {
+    chunksMap[hashOfChunkFootprint(chunksArrayElement["chunkFootprint"])] = chunksArrayElement;
+  }
+  return chunksMap;
+}
+
+
+/////
+// Server
+app.get('/', (req, res) => {
+  res.send('Hello from the map share plugin!');
+})
+
+app.get('/chunks', (req, res) => {
+  let mapData = JSON.parse(fs.readFileSync(mapFile));
+  res.send(Object.values(mapData));
+})
+
+app.post('/chunks', bodyParser.json(), (req, res) => {
+  // if the map file doesn't exist, create it
+  if (!fs.existsSync(mapFile)) {
+    fs.writeFileSync(mapFile, '{}');
+  }
+
+  // Convert chunks into internal map format
+  let chunksArray = req.body["chunks"];
+  let chunksMap = convertChunksArrayToChunkMap(chunksArray)
+  let mapData = JSON.parse(fs.readFileSync(mapFile));
+
+  // Add all chunks to current map
+  for (let chunkKey in chunksMap) {
+    mapData[chunkKey] = chunksMap[chunkKey];
+  }
+
+  fs.writeFileSync(mapFile, JSON.stringify(mapData));
+  res.send({success: true});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
